adc9240c05d101379bec0ab085a56f8a030ec8a5
Shivang-cx <wd63.department@gmail.com>
2024-10-18 19:44:34 +0530

    set header and banner section

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,35 +1,51 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   gsap.set('.cursor', { xPercent: -50, yPercent: -50, scale: 1, opacity: 1 });
 
   let cursor = document.querySelector('.cursor');
-  let buttons = document.querySelectorAll('.btn');
-  let secondSection = document.querySelector('.second-section'); // Replace with your actual second section selector
+  let buttons = document.querySelectorAll('header.cust-header ul li.menu-item a.menu-link');
+  let secondSection = document.querySelector('.banner-section'); // Verify this selector
+  let headerButton = document.querySelector('.CAT-button');
 
   let mouseX, mouseY;
 
   // Track mouse movement and move cursor
   window.addEventListener('mousemove', e => {
-      mouseX = e.clientX;
-      mouseY = e.clientY;
+    mouseX = e.clientX;
+    mouseY = e.clientY;
 
-      gsap.to(cursor, 0.5, { x: mouseX, y: mouseY });
+    gsap.to(cursor, { duration: 0.5, x: mouseX, y: mouseY });
   });
+
   // Button hover effects
   buttons.forEach(button => {
     button.addEventListener('mouseenter', () => {
-      gsap.to(cursor, 0.5, {
-        scale: 0,
-        opacity: 0
-      });
+      gsap.to(cursor, { duration: 0.5, backgroundColor: '#cfcfff', scale: 1 });
+      $(cursor).addClass("hovered");
     });
 
     button.addEventListener('mouseleave', () => {
-      gsap.to(cursor, 0.5, {
-        scale: 1,
-        opacity: 1,
-        x: mouseX,
-        y: mouseY
-      });
+      gsap.to(cursor, { duration: 0.5, backgroundColor: '#000', scale: 1 });
+      $(cursor).removeClass("hovered");
     });
+  });
+
+  secondSection.addEventListener('mouseenter', () => {
+    gsap.to(cursor, { duration: 0.5, backgroundColor: '#fff', scale: 5 });
+    $(cursor).addClass("hovered");
+  });
+
+  secondSection.addEventListener('mouseleave', () => {
+    gsap.to(cursor, { duration: 0.5, backgroundColor: '#000', scale: 1 });
+    $(cursor).removeClass("hovered");
+  });
+
+  headerButton.addEventListener('mouseenter', () => {
+    gsap.to(cursor, { duration: 0.5 , backgroundColor: '#ffff00', scale: 3});
+    $(cursor).addClass("hovered"); 
+  });
+
+  headerButton.addEventListener('mouseleave', () => {
+    gsap.to(cursor, { duration: 0.5 , backgroundColor: '#000', scale: 1 });
+    $(cursor).addClass("hovered");
   });
 });
